2e74423a0fbe007102098f7343a37428e7f49fe5
ghofran <gho0ofran.adel14@gmail.com>
2022-07-14 20:27:13 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+
+
+
+
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  
+  useEffect(() => {
+    const act = setTimeout(() => {
+      setTimeRemaining((timeRemaining)=>(timeRemaining-1)); 
+    }, 1000);
+    if (timeRemaining === 0) {
+       setTimeRemaining(10);
+          onAnswered(false);
+        }
+        return () => clearTimeout(act);
+          
+      
+  }, [timeRemaining]);
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
